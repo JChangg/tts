4c5f667ca10e508dad9f4ace91622b8fb8a55b04
JChangg <cc751@cam.ac.uk>
2016-03-23 21:55:26 +0000

    initial commit

---
+++ Server.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import tts
+
+app = Flask(__name__)
+
+
+@app.route('/speak/<text>', methods=['POST', 'GET'])
+def speak(text):
+    tts.add_text(text)
+    return jsonify({"content":text})
+
+
+if __name__ == '__main__':
+    tts.Consumer.start()
+    app.run(debug=True, host='0.0.0.0')
---
+++ tts.py
@@ -0,0 +1,24 @@
+from Queue import Queue
+from threading import Thread
+import pyttsx
+import time
+
+wait_time = 2
+
+engine = pyttsx.init()
+text_queue = Queue(10)
+
+
+def add_text(text):
+    text_queue.put(text)
+
+
+class Consumer(Thread):
+    def run(self):
+        global text_queue
+        while True:
+            text = text_queue.get()
+            text_queue.task_done()
+            engine.say(text)
+            engine.runAndWait()
+            time.sleep(wait_time)
