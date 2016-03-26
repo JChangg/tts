22e7f1399ab07e1f9c88245ed98b8f69a97b1bdb
JChangg <cc751@cam.ac.uk>
2016-03-26 13:24:45 +0000

    Fixed stylistic issues

--- Server.py
+++ Server.py
@@ -1,21 +1,24 @@
 from flask import Flask, jsonify
+
+import Music as mp
 import tts
 
 app = Flask(__name__)
 
+tts_server = tts.TextToSpeaker()
+music_player = mp.MusicPlayer()
+
 
 @app.route('/speak/<text>', methods=['POST', 'GET'])
 def speak(text):
-	
-	pos = tts.add_text(text)
-	if pos >= 0:
-		return jsonify({"content":text, "pos":pos})
-	else:
-		return jsonify({}), 503
+    pos = tts_server.add_text(text)
+    if pos >= 0:
+        return jsonify({"content": text, "pos": pos})
+    else:
+        return jsonify({}), 503
 
 
 if __name__ == '__main__':
-	consumer = tts.Consumer()
-	consumer.daemon = True
-	consumer.start()
-	app.run(debug=True, host='0.0.0.0')
+    tts_server.start()
+    music_player.start()
+    app.run(debug=True, host='0.0.0.0')
--- tts.py
+++ tts.py
@@ -1,34 +1,28 @@
 import Queue
 from threading import Thread
+
 import pyttsx
-import time
 
 wait_time = 0.5
 
-text_queue = Queue.Queue(5)
 
+class TextToSpeaker(Thread):
+    def __init__(self):
+        super(TextToSpeaker, self).__init__()
+        self.queue = Queue.Queue(5)
+        self.daemon = True
 
+    def add_text(self, text):
+        try:
+            self.queue.put(text, timeout=1)
+            return self.queue.qsize()
+        except Queue.Full:
+            return -1
 
-def add_text(text):
-	try:
-		text_queue.put(text, timeout=1)	
-		return text_queue.qsize()
-	except Queue.Full:
-		return -1
-class Consumer(Thread):
-	def __init__(self):
-		super(Consumer, self).__init__()
-
-	def run(self):
-		global text_queue
-		engine = pyttsx.init()
-		while True:		
-			text = text_queue.get()
-			text_queue.task_done()
-			engine.say(text)
-			engine.runAndWait()
-
-
-			
-
-
+    def run(self):
+        engine = pyttsx.init()
+        while True:
+            text = self.queue.get()
+            self.queue.task_done()
+            engine.say(text)
+            engine.runAndWait()
